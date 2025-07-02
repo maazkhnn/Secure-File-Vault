33c9a2bbf8eab1327b42a3d22fd9c7cd39f70489
Muhammad Maaz Khan <maazkhan@Muhammads-Laptop.local>
2025-07-02 13:25:02 -0400

    The first of many 💪

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const users = require('./routes/userRoutes');
+
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+    res.status(202).send(`Ay yo this port's working on ${PORT}`);
+});
+
+app.get('/:id', (req, res) => {
+    const { id } = req.params;
+    res.send(id);
+})
+
+app.use('/api/users', users);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost.com:${PORT}`);
+});
---
+++ routes/userRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json([{ id: 1, name: 'John Smith'}]); //res.send sends the same because of the nature of the content itself(its in JSON)
+});
+
+module.exports = router;
